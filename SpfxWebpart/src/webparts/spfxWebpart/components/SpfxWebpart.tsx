06c4784da1aca6697e1ec3de6d7de837eb8b8dd1
Sumedh Bhattacharya <sumbhatt@microsoft.com>
2021-08-12 09:26:44 -0700

    add fluid

--- SpfxWebpart/src/webparts/spfxWebpart/components/SpfxWebpart.tsx
+++ SpfxWebpart/src/webparts/spfxWebpart/components/SpfxWebpart.tsx
@@ -1,25 +1,78 @@
-import * as React from 'react';
-import styles from './SpfxWebpart.module.scss';
-import { ISpfxWebpartProps } from './ISpfxWebpartProps';
-import { escape } from '@microsoft/sp-lodash-subset';
+import * as React from "react";
+import styles from "./SpfxWebpart.module.scss";
+import { ISpfxWebpartProps } from "./ISpfxWebpartProps";
+import { escape } from "@microsoft/sp-lodash-subset";
+import {
+  FrsClient,
+  InsecureTokenProvider,
+} from "@fluid-experimental/frs-client";
+import { SharedMap } from "@fluid-experimental/fluid-framework";
 
-export default class SpfxWebpart extends React.Component<ISpfxWebpartProps, {}> {
-  public render(): React.ReactElement<ISpfxWebpartProps> {
-    return (
-      <div className={ styles.spfxWebpart }>
-        <div className={ styles.container }>
-          <div className={ styles.row }>
-            <div className={ styles.column }>
-              <span className={ styles.title }>Welcome to SharePoint!</span>
-              <p className={ styles.subTitle }>Customize SharePoint experiences using Web Parts.</p>
-              <p className={ styles.description }>{escape(this.props.description)}</p>
-              <a href="https://aka.ms/spfx" className={ styles.button }>
-                <span className={ styles.label }>Learn more</span>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
+const getContainerId = () => {
+  let isNew = false;
+  if (window.location.hash.length === 0) {
+    isNew = true;
+    window.location.hash = Date.now().toString();
   }
+  const containerId = window.location.hash.substring(1);
+  return { containerId, isNew };
+};
+
+const localConfig = {
+  tenantId: "local",
+  tokenProvider: new InsecureTokenProvider("tenantKey", { id: "userId" }),
+  orderer: "http://localhost:7070",
+  storage: "http://localhost:7070",
+};
+
+const getFluidMap = async () => {
+  const { containerId, isNew } = getContainerId();
+
+  const containerSchema = {
+    name: "cra-demo",
+    initialObjects: { mySharedMap: SharedMap },
+  };
+
+  const client = new FrsClient(localConfig);
+  const { fluidContainer } = isNew
+    ? await client.createContainer({ id: containerId }, containerSchema)
+    : await client.getContainer({ id: containerId }, containerSchema);
+  // returned initialObjects are live Fluid data structures
+  return fluidContainer.initialObjects.mySharedMap as SharedMap;
+};
+
+export default function SpfxWebpart(props: ISpfxWebpartProps) {
+  const [fluidMap, setFluidMap] = React.useState<SharedMap | undefined>();
+  const [viewData, setViewData] = React.useState({ time: "" });
+
+  React.useEffect(() => {
+    // Get/Create container and return live Fluid data
+    getFluidMap().then((data) => setFluidMap(data));
+  }, []);
+
+  React.useEffect(() => {
+    if (!fluidMap) return;
+
+    // sync Fluid data into view state
+    const syncView = () => setViewData({ time: fluidMap.get("time") });
+    // ensure sync runs at least once
+    syncView();
+    // update state each time our map changes
+    fluidMap.on("valueChanged", syncView);
+    return () => {
+      fluidMap.off("valueChanged", syncView);
+    };
+  }, [fluidMap]);
+
+  if (!viewData) return <div />;
+
+  // business logic could be passed into the view via context
+  const setTime = () => fluidMap.set("time", Date.now().toString());
+
+  return (
+    <div>
+      <button onClick={setTime}> click </button>
+      <span>{viewData.time}</span>
+    </div>
+  );
 }
